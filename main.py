261aee77f669059c1a538d11a2005e6a43777a0b
Jen Russell <jennifer@jrussellux.com>
2024-10-05 13:01:57 -0700

    one

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import random
+import string
+
+
 result = ""
+# Function to get a random character/digit
+def rand_char():
+    possibilities = string.ascii_lowercase + string.ascii_uppercase + string.digits
+    return random.choice(possibilities)
 
 for i in range(10):
-    result += "a"
+    result += rand_char()
 
 print(result)
+
