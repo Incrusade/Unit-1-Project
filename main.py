38d702be601ef960c173973d700b7a00803b89e3
Incrusade <phenae925@gmail.com>
2024-10-05 14:11:21 -0700

    Add branch 2

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+#
+def get_count():
+    value = int(input("Pick A Number: "))
+    return value 
+
+count = get_count()
+
 result = ""
 
-for i in range(10):
+for i in range(count):
     result += "a"
 
 print(result)
+
+
